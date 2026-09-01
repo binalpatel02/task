4f4d92e20d6c21d2c8e4367e5012f52a97c30563
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-01 13:19:43 +0530

    add userId and user object

--- src/module/customer/interface/customer.interface.ts
+++ src/module/customer/interface/customer.interface.ts
@@ -1,7 +1,18 @@
+import mongoose from "mongoose";
+
 export interface ICustomer {
-  firstName: string;
-  lastName: string;
-  countryCode: string;
-  mobileNumber: string;
-  emailAddress?: string;
+    firstName: string;
+    lastName: string;
+    countryCode: string;
+    mobileNumber: string;
+    emailAddress?: string;
+    userId: mongoose.Schema.Types.ObjectId;
+    
+    user: {
+      id: string;
+      first_name: string;
+      last_name: string;
+      email_address: string;
+      mobile_number: string;
+    }
 }
--- src/module/customer/model/schema/customer.schema.ts
+++ src/module/customer/model/schema/customer.schema.ts
@@ -33,7 +33,21 @@ const customerSchema = new Schema<ICustomer>(
       trim: true,
       lowercase: true,
       unique: true
-   }
+   },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+   },
+
+    user: {
+      id: { type: String },
+      first_name: { type: String },
+      last_name: { type: String },
+      email_address: { type: String },
+      mobile_number: { type: String }
+    }
+
   },
   {
     timestamps: true
