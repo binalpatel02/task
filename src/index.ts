10fc421d0462430f3591abfff946f414c66362ea
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-02 14:03:01 +0530

    add consumer

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,30 @@
 import "dotenv/config";
-import "./library/passport.js"; 
-import { connectionRedis } from "./library/redis.js";
-import { startCustomerSyncConsumer } from "./module/customer/rabbitmq/consumer.js";
-
 import mongoose from "mongoose";
 import app from "./app.js";
+import { connectionRedis } from "./library/redis.js";
+import { startUserConsumer } from "./module/user/library/rabbitmq/user.consumer.js";
 
 const PORT = process.env.PORT || 3000;
-const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/customer-master';
 
 const startServer = async () => {
-  try {
-    if (!mongoURI) {
-      throw new Error("MONGO_URI is not defined");
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+
+        console.log( `MongoDB connected: ${process.env.INSTANCE}`);
+
+        await connectionRedis();
+
+        await startUserConsumer();
+
+        app.listen(PORT, () => {
+            console.log(`${process.env.INSTANCE} running on port ${PORT}`);
+        });
     }
-
-    await mongoose.connect(mongoURI);
-    console.log("MongoDB connected");
-
-    await connectionRedis();
-    console.log("Redis connected");
-
-    await startCustomerSyncConsumer();
-    console.log("RabbitMQ Consumers initialized");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  }
-  
-  catch (error) {
-    console.error("Server startup failed:", error);
-    process.exit(1);
-  }
+    
+    catch (error) {
+        console.error("Server startup failed:", error);
+        process.exit(1);
+    }
 };
 
 startServer();
